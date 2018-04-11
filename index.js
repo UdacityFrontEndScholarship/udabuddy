daf5a9176fcf4b453f7f8d06e7462267d14406fd
Unknown <avinashjaiswal642@gmail.com>
2018-04-11 23:17:08 +0530

    My simple code

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const hbs=require('hbs');
+const app = express();
+const path=require('path');
+const port=process.env.PORT||3000;
+
+app.set('view engine', 'hbs');
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/',(req,res)=>{
+    res.render('home');
+})
+
+app.listen(port,()=>{
+    console.log('Listening on port 3000');
+})
