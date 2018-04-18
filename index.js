fcfc5ebe66ab4bf2e360948c909fab5feb7716ee
Nilesh Solanki <nileshsolanki456@gmail.com>
2018-04-18 19:03:14 +0530

    fix: add link to resources and profile

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const port=process.env.PORT||3000;
 app.set('view engine', 'hbs');
 app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'media')));
 
 
 app.get('/',(req,res)=>{
