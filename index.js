0fca035a56d4421e1a35c1eb759bcc1d4eb12742
Unknown <avinashjaiswal642@gmail.com>
2018-04-26 21:19:17 +0530

    Improved file structure

--- index.js
+++ index.js
@@ -21,6 +21,36 @@ app.get('/profile',(req,res)=>{
 app.get('/resources',(req,res)=>{
     res.render('resources');
 });
+app.get('/about',(req,res)=>{
+    res.render('about');
+});
+app.get('/blogDemo',(req,res)=>{
+    res.render('blogDemo');
+});
+app.get('/wofDemo',(req,res)=>{
+    res.render('wofDemo');
+});
+app.get('/blog',(req,res)=>{
+    res.render('blog');
+});
+app.get('/wof',(req,res)=>{
+    res.render('wof');
+});
+app.get('/signIn',(req,res)=>{
+    res.render('signIn');
+});
+app.get('/forum',(req,res)=>{
+    res.render('forum');
+});
+app.get('/showcase',(req,res)=>{
+    res.render('showcase');
+});
+app.get('/weekUpdates',(req,res)=>{
+    res.render('weekUpdates');
+});
+app.get('/about',(req,res)=>{
+    res.render('about.hbs');
+});
 app.listen(port,()=>{
     console.log('Listening on port 3000');
 });
---
+++ public/js/wofD.js
@@ -0,0 +1,29 @@
+var config = {
+    apiKey: "AIzaSyC18bxhVaauMyqjk4fvZkiUZkCU5qB9mDc",
+    authDomain: "udabuddy.firebaseapp.com",
+    databaseURL: "https://udabuddy.firebaseio.com",
+    projectId: "udabuddy",
+    storageBucket: "",
+    messagingSenderId: "460323300752"
+  };
+  firebase.initializeApp(config);
+
+
+  initApp = function() {
+    firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+        // User is signed in.
+        // Please write your code inside here
+        window.location.replace('/wof');
+      } else {
+        // User is signed out.
+        
+      }
+    }, function(error) {
+      console.log(error);
+    });
+  };
+
+  window.addEventListener('load', function() {
+    initApp()
+  });
