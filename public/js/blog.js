758f35a9f8b5e2a378971d2cee5c7e39ee9b5468
Sourya Varanasi <sourya0411@gmail.com>
2018-05-10 02:47:35 +0530

    style-blog-page

--- public/js/blog.js
+++ public/js/blog.js
@@ -11,6 +11,19 @@ firebase.initializeApp(config);
 
 
 initApp = function () {
+  $(document).ready(function(){
+  $('.button-collapse').sideNav({
+      menuWidth: 300, // Default is 300
+      edge: 'left', // Choose the horizontal origin
+      closeOnClick: false, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true // Choose whether you can drag to open on touch screens
+    }
+  );
+  // START OPEN
+  $('.button-collapse').sideNav('show');
+});
+
+
   firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
       // User is signed in.
